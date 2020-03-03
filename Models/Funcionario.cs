adae915eed13e060f9e66f52852db75bf66e9f27
LUCAS ELMER MARTINS <lucas.martins2@alunos.sc.senac.br>
2020-03-02 22:04:44 -0300

    02/03

---
+++ Models/Funcionario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace mvc.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    
+        
+    }
+
+}
